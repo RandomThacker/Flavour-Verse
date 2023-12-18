ffe694ec23167449ed82ce865616c3f8c3652c69
RandomThacker <randomthacker@gmail.com>
2023-12-18 15:04:26 +0530

    react router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Navbar from "./Components/Navbar";
 import Home from "./pages/Home";
+import { Routes, Route, Outlet } from "react-router-dom";
 import RecipeDetail from "./pages/RecipeDetail";
+
+function Layout(){
+  return(
+    <>
+    <Navbar/>
+    <Outlet/>
+    <Footer/>
+    </>
+  )
+}
 
 export default function App() {
   return (
     <div className="bg-black">
-      <Home/>
+      <Routes>
+        <Route path="/" element={<Layout/>}/>
+        <Route index element={<Home/>} />
+        <Route path="recipes/:id" element = {<RecipeDetail/>} />
+      </Routes>
     </div>
   )
 }
