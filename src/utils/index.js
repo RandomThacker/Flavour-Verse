7b97d7e66eb2ba0379ee3215648a7cd1674e19b7
RandomThacker <randomthacker@gmail.com>
2023-12-27 15:51:17 +0530

    add : recipeAdd

--- src/utils/index.js
+++ src/utils/index.js
@@ -13,7 +13,7 @@ export async function fetchRecipes(filter) {
 }
 
 export async function fetchRecipe(id){
-    const url = `https://api.edamam.com/search?r=http://www.edamam.com/ontologies/edamam.owl%23${id}&app_id=${process.env.REACT_APP_EDAMAM_APP_ID}&app_key=${process.env.REACT_APP_EDAMAM_API_KEY}`
+    const url = `https://api.edamam.com/search?r=http://www.edamam.com/ontologies/edamam.owl%23${id}&app_id=${REACT_APP_EDAMAM_APP_ID}&app_key=${REACT_APP_EDAMAM_API_KEY}`
     
     const response = await fetch(url)
     
